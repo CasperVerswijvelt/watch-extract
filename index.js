96c6940c5bdc006fc76f1de7ae8659869430f6f3
CasperVerswijvelt <casperverswijvelt@gmail.com>
2020-12-28 22:45:53 +0100

    Use pure js libraries for both unrarring and unzipping

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import chokidar from "chokidar";
 import fs from "fs";
 import path from "path";
-import Seven from "node-7z";
+
+import extract from "extract-zip";
 import unrar from "node-unrar-js";
+
 import ConsoleStamp from "console-stamp";
 
 ConsoleStamp(console);
@@ -11,6 +13,7 @@ ConsoleStamp(console);
 
 const args = process.argv.slice(2);
 
+// Default to current directory
 let watchDirectory = ".";
 
 if (args.length > 0 && typeof args[0] === "string") watchDirectory = args[0];
@@ -19,14 +22,22 @@ if (args.length > 0 && typeof args[0] === "string") watchDirectory = args[0];
 
 const TYPE_ZIP = ".zip";
 const TYPE_RAR = ".rar";
+const TYPE_TAR_GZ = ".tar.gz";
+const TYPE_GZ = ".gz";
+
+// Name of folder to extract to
 
 const EXTRACTED = "extracted";
 
+// Watch added files
 chokidar.watch(watchDirectory).on("add", async (filePath) => {
+  // Ignore root directory
   if (filePath === watchDirectory) return;
 
   const isZip = filePath.endsWith(TYPE_ZIP);
   const isRar = filePath.endsWith(TYPE_RAR);
+  const isTarGz = filePath.endsWith(TYPE_TAR_GZ);
+  const isGz = filePath.endsWith(TYPE_GZ) && !isTarGz;
 
   if (isZip || isRar) {
     const parentFolder = path.dirname(filePath);
@@ -36,6 +47,7 @@ chokidar.watch(watchDirectory).on("add", async (filePath) => {
     console.log(`Found supported archive file ${filePath}`);
     console.log(`Extracted folder exists: ${exists}`);
 
+    // If extracted folder already exists, return
     if (exists) return;
 
     const logProgressZip = (progress) =>
@@ -43,19 +55,18 @@ chokidar.watch(watchDirectory).on("add", async (filePath) => {
     const logDataZip = (data) =>
       console.log(`Extracted data: ${data.file}: ${data.status}`);
 
-    const logProgressRar = (percent) => {
-      console.log(`Extracting ${filePath} progress: ${percent}`);
-    };
-
     const logEnd = () =>
       console.log(`Succesfully extracted ${filePath} to ${extractedFolder}`);
     const logError = (error) => {
       console.error(`Error while extracting ${filePath}: ${error}`);
     };
 
+    // Make sure extracted folder is created before extracting
+    fs.mkdirSync(extractedFolder);
+
+    // Type dependent extracting
     if (isRar) {
-      console.log(`Extracting archive using unrar`);
-      fs.mkdirSync(extractedFolder);
+      console.log(`Extracting .rar archive`);
 
       const extractor = unrar.createExtractorFromFile(
         filePath,
@@ -69,17 +80,18 @@ chokidar.watch(watchDirectory).on("add", async (filePath) => {
         logEnd();
       }
     } else if (isZip) {
-      console.log(`Extracting archive using 7zip`);
+      console.log(`Extracting .zip archive`);
+
       try {
-        const stream = Seven.extractFull(filePath, extractedFolder, {
-          $progress: true,
-        });
-        stream.on("end", logEnd);
-        stream.on("progress", logProgressZip);
-        stream.on("data", logDataZip);
+        await extract(filePath, { dir: path.resolve(extractedFolder) });
+        logEnd();
       } catch (e) {
         logError(e);
       }
+    } else if (isTarGz) {
+      logError("Not yet implemented");
+    } else if (isGz) {
+      logError("Not yet implemented");
     }
   }
 });
